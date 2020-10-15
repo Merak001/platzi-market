f518866c6a1a1b146cbf374e2060c51f2d1f11d6
Tijua Tejeda <tejeda.morales.jose.manuel@gmail.com>
2020-10-15 01:45:15 -0500

    Se agrega docmunntacion al archivo properties, se crea la estructura del proyecto

--- src/main/java/com/puchungis/platzimarket/web/controller/HolamundoController.java
+++ src/main/java/com/puchungis/platzimarket/web/controller/HolamundoController.java
@@ -1,4 +1,4 @@
-package com.puchungis.platzimarket;
+package com.puchungis.platzimarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,5 +16,6 @@ public class HolamundoController {
 		return "Nunca dejes de intentarlo";
 	 
 	}
+
 	
 }
